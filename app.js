9aeae67231d947b512c8d09e9fb50dd35bd0918e
omardr777 <omarcoursee@gmail.com>
2021-12-09 03:19:00 +0300

    make it better

--- app.js
+++ app.js
@@ -224,7 +224,7 @@ mongoose
             //     })
             // }
         });
-        app.listen(3000);
+        app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
